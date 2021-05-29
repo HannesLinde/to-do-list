6115cd71e4423676377763199a1462dad651b73f
Hannes Lindenberg <hanneslinde@posteo.de>
2021-05-29 09:24:27 +0000

    add basic functionality: create todo and list todos

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,14 +1,62 @@
 <template>
   <div>
     <h2 class="text-2xl">Create New To Do</h2>
-    <input type="text" placeholder="Title" />
+    <form
+      @submit.prevent="submit"
+      class="flex flex-col place-items-center space-y-4"
+    >
+      <input
+        type="text"
+        name="title"
+        placeholder="Title To Do"
+        v-model="title"
+        class="bg-yellow-100 text-center w-1/2 border border-gray-400 rounded-md"
+      />
+      <input
+        type="text"
+        name="content"
+        placeholder="Content To Do"
+        v-model="content"
+        class="bg-yellow-100 text-center w-1/2 border border-gray-400 rounded-md"
+      />
+      <button
+        type="submit"
+        class="bg-yellow-500 hover:bg-yellow-100 border border-gray-200 hover:border-gray-400 rounded-md px-4"
+      >
+        Save
+      </button>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import axios from "axios";
+const now = new Date();
 
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return {
+      title: "",
+      content: "",
+    };
+  },
+  methods: {
+    async submit() {
+      console.log("submit");
+      try {
+        const response = await axios.post("http://localhost:3000/todos", {
+          title: this.title,
+          content: this.content,
+          createdAt: now,
+        });
+        console.log(response);
+      } catch (error) {
+        console.log("Sorry, an error occured: " + error);
+      }
+    },
+  },
+});
 </script>
 
 <style scoped>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,13 +1,36 @@
 <template>
   <div>
     <h2 class="text-2xl">All my To Dos</h2>
+    <div v-if="todos.length == 0">Nothing to do. Whooohooo!</div>
+    <div v-for="todo in todos" :key="todo.id">
+      <div class="text-xl">{{ todo.title }}</div>
+      <div>{{ todo.content }}</div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import axios from "axios";
 
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  async mounted() {
+    console.log("load list");
+    this.listToDos();
+  },
+  methods: {
+    async listToDos() {
+      const response = await axios.get("http://localhost:3000/todos");
+      this.todos = response.data;
+      console.log(response.data);
+    },
+  },
+});
 </script>
 
 <style scoped>
